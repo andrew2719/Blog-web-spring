9b326573ed66aeabbf81d60293180d36736a28c1
andrew2719 <andrewblaze2719@gmail.com>
2024-05-13 00:10:30 +0530

    update options are also working

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -3,14 +3,17 @@ package com.example.demo.controller;
 import com.example.demo.model.Blogs;
 import com.example.demo.model.User;
 import com.example.demo.service.BlogService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.apache.tomcat.util.modeler.BaseAttributeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -27,28 +30,54 @@ public class BlogController {
         return "new_blog";
     }
 
-    @PostMapping("/blogs")
-    public String addBlog(@ModelAttribute("blog") Blogs blog,Model model, HttpSession session) {
-        User user = (User) session.getAttribute("user");
-        blog.setUser(user);
-        blogService.addBlog(blog);
+//    @PostMapping("/blogs")
+//    public String addBlog(@ModelAttribute("blog") Blogs blog,Model model, HttpSession session) {
 //        User user = (User) session.getAttribute("user");
-        List<Blogs> blogs_of_the_user = blogService.getBlogsByUser(user);
-        model.addAttribute("blogs", blogs_of_the_user);
-        return "blogs_user";
+//        blog.setUser(user);
+//        blogService.addBlog(blog);
+////        User user = (User) session.getAttribute("user");
+//        List<Blogs> blogs_of_the_user = blogService.getBlogsByUser(user);
+//        model.addAttribute("blogs", blogs_of_the_user);
+//        return "blogs_user";
+//    }
+    @RequestMapping(value = "/blogs", method = {RequestMethod.GET, RequestMethod.POST})
+    public String manageBlogs(@ModelAttribute("blog") Blogs blog, BindingResult result, Model model, HttpSession session, HttpServletRequest request) {
+        if (request.getMethod().equalsIgnoreCase("POST") && !result.hasErrors()) {
+            User user = (User) session.getAttribute("user");
+            blog.setUser(user);
+            blogService.addBlog(blog);
+            return "redirect:/blogs";  // Redirect to avoid duplicate form submission
+        } else {
+            User user = (User) session.getAttribute("user");
+            if (user != null) {
+                List<Blogs> blogs = blogService.getBlogsByUser(user);
+                model.addAttribute("blogs", blogs);
+            }
+            return "blogs_user";  // This view shows the blogs or the form to add new blogs
+        }
     }
+
 
     @GetMapping("/blogs/edit/{id}")
     public String editBlog(@PathVariable Long id, Model model) {
-        model.addAttribute("blog", blogService.getBlog(id));
-        return "edit_blog";
+        Optional<Blogs> blog = blogService.getBlog(id);
+        if (blog.isPresent()) {
+            model.addAttribute("blog", blog.get());
+        } else {
+            return "redirect:/blogs";  // Redirect if the blog is not found
+        }
+        return "edit_blog";  // Name of the HTML file
     }
 
+
     @PostMapping("/blogs/update")
-    public String updateBlog(@ModelAttribute("blog") Blogs blog) {
-        blogService.editBlog(blog);
-        return "redirect:/blogs";
+    public String updateBlog(@ModelAttribute("blog") Blogs blog, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        blog.setUser(user);
+        blogService.editBlog(blog);  // Save changes to the database
+        return "redirect:/blogs";  // Redirect to avoid duplicate submissions
     }
+
 
     @GetMapping("/blogs/delete/{id}")
     public String deleteBlog(@PathVariable Long id) {
